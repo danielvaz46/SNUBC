faad0e1373efcb62c3579dd6127516ab7d4964c0
Daniel David Vaz <49115175+danielvaz46@users.noreply.github.com>
2019-08-27 11:58:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var deadline = new Date("Oct 19, 2019 8:30:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+$('.days').text(days);
+$('.hours').text(hours);
+$('.mins').text(minutes);
+$('.secs').text(seconds);
+}, 1000);
